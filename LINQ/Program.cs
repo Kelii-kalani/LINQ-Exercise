871cf6821d570edc1aa8353a3c46f41b9c18e6be
Michael <keliikalani87@gmail.com>
2023-02-22 11:21:49 -0800

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> games = new List<string>() { "Grand Turismo", "Call of duty", "Twisted Metal", "Tekken tag", "NBA 2K", "Ace Combat" };
+
+            games.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
         }
     }
 }
